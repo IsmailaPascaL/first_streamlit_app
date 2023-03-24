45c718b2ce97262667cacafcc25e7ff4fcec0395
IsmailaPascaL <52896457+IsmailaPascaL@users.noreply.github.com>
2023-03-24 16:56:59 +0000

    Update streamlit_app.py
    fix load list not working

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,8 @@ streamlit.header("The fruit load list contains:")
 
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-      my_cur.fetchall()
+      my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+      return my_cur.fetchall()
 
 #ADD BUTTON TO LOAD FRUIT
 if streamlit.button('Get Fruit Load List'):
