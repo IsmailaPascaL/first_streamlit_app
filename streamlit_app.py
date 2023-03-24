419d0a8d49575c9a6f33e9760d612110d0e702c7
IsmailaPascaL <52896457+IsmailaPascaL@users.noreply.github.com>
2023-03-24 16:25:22 +0000

    Update streamlit_app.py
    Added Try Except in advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 #Fruityvice advice response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.write('The user entered', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
+ except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -46,5 +48,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
-add_fruit = streamlit.text_input('What fruit would you like to add?', '')
+add_fruit = streamlit.text_input('What fruit would you like to add?')
 my_cur.execute("insert into fruit_load_list values('')")
